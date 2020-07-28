b56bf732e5d83e484680df3f65b2514f15804973
susifu <susanamg1988@hotmail.com>
2020-07-28 10:39:33 +0200

    primer commit

---
+++ app/Http/Controllers/Admin/TagController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Request\TagStoreRequest;
+use App\Http\Request\TagUpdateRequest;
+
+use App\Http\Controllers\Controller;
+
+use App\Tag;
+
+class TagController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {   
+       //se muestra toda la lista de etiquetas 
+        $tags = Tag::orderBy('id', 'DESC')->paginate();
+        // dd($tags);
+        return view('admin.tags.index', compact('tags'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //muestra el formulario
+        return view('admin.tags.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(TagStoreRequest $request)
+    {
+
+        $tag = Tag::create($request->all());
+
+       
+       return redirect()->route('tags.edit', $tag->id)
+       ->with('info', 'Etiqueta creada con éxito');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //ver en detalle una etiqueta
+        $tag = Tag::find($id);
+
+        return view('admin.tags.show', compact('tag'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //permite editar una etiqueta 
+        $tag = Tag::find($id);
+        return view('admin.tags.edit', compact('tag'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(TagUpdateRequest $request, $id)
+    {
+        //actualiza la vista de los datos modificados 
+        $tag = Tag::find($id);
+
+        $tag->fill($request->all())->save();
+
+        return redirect()->route('tags.edit', $tag->id)
+        ->with('info', 'Etiqueta modificada con éxito');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //Eliminamos un registro 
+        $tag = Tag::find($id)->delete();
+        
+        return back()->with('info', 'Etiqueta eliminada correctamente');
+
+    }
+}
